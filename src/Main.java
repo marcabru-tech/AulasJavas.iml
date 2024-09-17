2f419aa3c8538a76147c0cfe46b6ed1134122bd1
marcabru-tech <dommarcabrugmail.com>
2024-09-17 17:02:17 -0300

    Exercícios da Aula 04, sobre if, else if, else, e switch. Usei o ctrl + alt + l para formatar o código. treino commit and push fiz a organização das pastas conforme indicado: Aulas Java --> Aula 02 e Aula 04 e uma pasta de exercicios para cada aula.

--- src/Main.java
+++ src/Main.java
@@ -146,6 +146,35 @@ public class Main {
                 }
             }
 
+            //Ex.3 da Aula 02
+
+            import java.util.Scanner
+            public class Exemplos {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Digite seu nome: ");
+
+                String nome = scanner.nextLine();
+
+
+                System.out.println("Digite sua idade: ");
+                int idade = scanner.nextInt();
+
+                System.out.println("Digite sua altura: ");
+                double altura = scanner.nextDouble();
+
+                System.out.println("Seu nome é " + nome);
+                System.out.println("Sua idade é: " + idade);
+                System.out.println("Sua altura é: " + altura);
+            }
+
+            }
+// Fiz a organização em pastas conforme sugerido
+            }
+        }
+
+
 
 
 
